994d6e45602a73c699119874347b060af676b808
Link <sandlincoln@gmail.com>
2019-08-31 20:44:23 -0700

    switched it to use a controller. Will test it in class.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,11 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot{
+    private XboxController controller = new XboxController(0);
+
 	private DigitalInput limitSwitch = new DigitalInput(6);
 	private final double[] setpointList = {64.0, 75.0, 98.5, 130.0, 140.0};
 	private final double offset = 19.0;
@@ -19,8 +17,8 @@ public class Robot extends IterativeRobot{
 	private Spark overMotor = new Spark(3);
 	private DifferentialDrive m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
 	
-	private Joystick m_leftStick = new Joystick(0);
-	private Joystick m_rightStick = new Joystick(1);
+	//private Joystick m_leftStick = new Joystick(0);
+	//private Joystick m_rightStick = new Joystick(1);
 	
 	private Encoder armEncoder = new Encoder(0,1,false,Encoder.EncodingType.k4X);
 	
@@ -46,8 +44,11 @@ public class Robot extends IterativeRobot{
 	@Override
 	public void teleopPeriodic() {
 		System.out.println((-armEncoder.get()/toAngle + offset) + " " + (pidObj.getSetpoint()));
-		m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
-		//liftMotor.set(1);//pulses per sec / pulses per revolution / 60
+		//m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+
+        m_myRobot.tankDrive(-controller.getY(GenericHID.Hand.kLeft), -controller.getY(GenericHID.Hand.kRight));
+
+        //liftMotor.set(1);//pulses per sec / pulses per revolution / 60
 		//System.out.println(armEncoder.getDistance());
 
 		if(!limitSwitch.get()){
@@ -58,43 +59,59 @@ public class Robot extends IterativeRobot{
 			armEncoder.setDistancePerPulse(1);
 		}
 
-		if(m_leftStick.getRawButton(2)){
+
+        //    4(270=left)(X=right)     3(0=left)(Y=right)    5(90=left)(B=right)
+        //          2(180=left)(A=right)
+
+		//if(m_leftStick.getRawButton(2)){
+		if(controller.getPOV() == 180){
 			setMotorVoltage(0.43, ((armEncoder.getRate() / 3) * 60));
 			//liftMotor2.set((0.2987));
 			//liftMotor.set(-(0.2987));
 			
 		}
-		else if(m_rightStick.getRawButton(4)){
+		//else if(m_rightStick.getRawButton(4)){
+		else if(controller.getXButton()){
 			setMotorVoltage(0.17, ((armEncoder.getRate() / 3) * 60));
 		}
+
 		//else if(m_rightStick.getRawButton(2)){
 		//	lifting = false;
 		//	lowering = true;
 		//	setMotorVoltage(-1, ((armEncoder.getRate() / 3) * 60));
 		//}
-		else if(m_rightStick.getRawButton(2)){
+
+		//else if(m_rightStick.getRawButton(2)){
+		else if(controller.getAButton()){
 			//setMotorVoltage(-.15, ((armEncoder.getRate() / 3) * 60));
 			//armEncoder.reset();
 			liftMotor.set(0.25);
 			liftMotor2.set(-0.25);
 		}
-		else if(m_leftStick.getRawButton(4)){
+
+		//else if(m_leftStick.getRawButton(4)){
+		else if(controller.getPOV() == 270){
 			setSetpoint(setpointList[0]);
 			pidVolts();
 		}
-		else if(m_leftStick.getRawButton(3)){
+		//else if(m_leftStick.getRawButton(3)){
+		else if(controller.getPOV() == 0){
 			setSetpoint(setpointList[1]);
 			pidVolts();
 		}
-		else if(m_rightStick.getRawButton(3)){
+		//else if(m_rightStick.getRawButton(3)){
+		else if(controller.getYButton()){
 			setSetpoint(setpointList[2]);
 			pidVolts();
 		}
-		else if(m_rightStick.getRawButton(5)){
+
+		//else if(m_rightStick.getRawButton(5)){
+		else if(controller.getBButton()){
 			setSetpoint(setpointList[3]);
 			pidVolts();
 		}
-		else if(m_leftStick.getRawButton(5)){
+		//else if(m_leftStick.getRawButton(5)){
+		else if(controller.getPOV() == 90){
 			setSetpoint(setpointList[4]);
 			pidVolts();
 		}
@@ -108,11 +125,14 @@ public class Robot extends IterativeRobot{
 		//else{
 		//	setMotorVoltage(0,0);
 		//}
-		if(m_leftStick.getTrigger()){
+
+        //if(m_leftStick.getTrigger()){
+		if(controller.getBumper(GenericHID.Hand.kLeft)){
 			overMotor.set(0.3333333333333333);
 			System.out.println("button 2 left pressed");
 		}
-		else if(m_rightStick.getTrigger()){
+		//else if(m_rightStick.getTrigger()){
+		else if(controller.getBumper(GenericHID.Hand.kRight)){
 			overMotor.set(-1);
 			System.out.println("button 2 pressed right");
 		}
@@ -123,7 +143,7 @@ public class Robot extends IterativeRobot{
 		System.out.println(liftMotor.get() + " " + liftMotor2.get());
 	}
 	
-	public void setMotorVoltage(double v, final double w) {
+	private void setMotorVoltage(double v, final double w) {
     //w = encoder rpm
     // R (resistance) and Ke are motor constants for the Redline/775 pro
 		//WHAT IS Ke?: (According to Vex 775 Data Sheet ) At 18730 RPM 
